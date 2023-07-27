cbc99f1c67bf608a79198ae2261845754fae6a2b
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-07-27 23:41:41 +0900

    [feat] add Setting model

--- Week3/SettingViewController.swift
+++ Week3/SettingViewController.swift
@@ -7,6 +7,31 @@
 
 import UIKit
 
+enum Setting: CaseIterable {
+    case general
+    case personal
+    case etc
+    
+    var korean: String {
+        switch self {
+        case .general: return "전체설정"
+        case .personal: return "개인설정"
+        case .etc: return "기타설정"
+        }
+    }
+    
+    static subscript(_ title: Setting) -> [String] {
+        switch title {
+        case .general: return  ["공지사항", "실험실", "버전정보"]
+        case .personal: return  ["개인/보안", "알림", "채팅", "멀티프로필"]
+        case .etc: return ["고객센터/도움말"]
+        }
+    }
+    
+    static let titleCount = Self.allCases.count
+}
+
+
 class SettingViewController: UITableViewController {
 
     override func viewDidLoad() {
